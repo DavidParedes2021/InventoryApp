0bc8c105db06d4053ddcb938548801207343d6b3
Naii-the-Baf <sfabian200@gmail.com>
2023-10-29 16:36:46 -0600

    Fix crash when loading SQLiteHelper

--- app/src/main/java/com/example/secondproject/MainActivity.kt
+++ app/src/main/java/com/example/secondproject/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        sqLiteHelper = SQLiteHelper(context = applicationContext)
 
         val productsList = sqLiteHelper.getProducts()
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
