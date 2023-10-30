f5af99082c767873e754cff0868a20a892394024
David Walls <83623630+DavidParedes2021@users.noreply.github.com>
2023-10-29 22:59:10 -0600

    Fixed EditActivity.kt to show the current values of the record, and fixed the color of button in activity_main.xml

--- app/src/main/java/com/example/secondproject/EditActivity.kt
+++ app/src/main/java/com/example/secondproject/EditActivity.kt
@@ -2,17 +2,25 @@ package com.example.secondproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.BaseColumns
 import android.widget.Toast
 import com.example.secondproject.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditBinding
+    private var productId: Int = -1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        productId = intent.getIntExtra("productId", -1)
+        val productName = intent.getStringExtra("productName") ?: ""
+        val productPrice = intent.getDoubleExtra("productPrice", 0.0)
+        val productQuantity = intent.getIntExtra("productQuantity", 0)
+
+        binding.NameInput.editText?.setText(productName)
+        binding.PriceInput.editText?.setText(productPrice.toString())
+        binding.QuantityInput.editText?.setText(productQuantity.toString())
 
         binding.EditButton.setOnClickListener {
             val name = binding.NameInput.editText?.text?.toString() ?: ""
@@ -25,28 +33,9 @@ class EditActivity : AppCompatActivity() {
 
 
 
-            var db_helper = SQLiteHelper(context = applicationContext)
-            val db_write = db_helper.writableDatabase
+            val db_helper = SQLiteHelper(context = applicationContext)
 
-
-            val cursor = db_write.query(
-                DBContract.FeedEntry.TABLE_NAME,   // The table to query
-                null,             // The array of columns to return (pass null to get all)
-                "id=?",            // The columns for the WHERE clause
-                arrayOf(name),    // The values for the WHERE clause
-                null,                   // don't group the rows
-                null,                   // don't filter by row groups
-                null              // The sort order
-            )
-
-            var Productid = 0
-            with(cursor) {
-              moveToNext()
-                Productid= getInt(getColumnIndexOrThrow(BaseColumns._ID))
-
-            }
-            cursor.close()
-            db_helper.update(Productid, name, price.toDouble(), quantity.toInt())
+            db_helper.update(productId, name, price.toDouble(), quantity.toInt())
             finish()
             }
         }
